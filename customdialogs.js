ef5b8f71c37661d264c4f40a1b0d290e510f6194
Thomas Fischer <tfischer@ucsd.edu>
2023-03-02 17:28:13 -0800

    Create repo

---
+++ customdialogs.js
@@ -0,0 +1,60 @@
+export function createDialog(contents, inputs = `<button type="submit">OK</button>`) {
+    let template = `
+    <form method="dialog">
+    ${contents}
+    ${inputs}
+    </form>`;
+
+    let dialog = document.createElement('dialog');
+    dialog.innerHTML = template;
+    return dialog;
+}
+
+export function createAlert(message) {
+    let contents = `<p>${message}</p>`;
+    let dialog = createDialog(contents);
+    return dialog;
+}
+
+export function createConfirm(message) {
+    let contents = `<p>${message}</p>`;
+    let inputs = `<button type="submit">Cancel</button>
+    <button type="submit" value="true">OK</button>`;
+
+    return createDialog(contents, inputs);
+}
+
+export function createPrompt(message) {
+    let contents = `<label for="prompt-dialog-input" id="prompt-dialog-message">${message}</label><br>`;
+    let inputs = `
+    <input id="prompt-dialog-input" name="prompt-dialog-input"><br>
+    <button type="submit">Cancel</button>
+    <button type="submit">OK</button>`;
+
+    return createDialog(contents, inputs);
+}
+
+/*
+<dialog id="alert-dialog">
+    <form method="dialog">
+        <p id="alert-dialog-message"></p>
+        <button type="submit">OK</button>
+    </form>
+</dialog>
+
+<dialog id="confirm-dialog">
+    <form method="dialog">
+        <p id="confirm-dialog-message"></p>
+        <button type="submit">Cancel</button>
+        <button type="submit" value="true">OK</button>
+    </form>
+</dialog>
+
+<dialog id="prompt-dialog">
+    <form method="dialog">
+        <label for="prompt-dialog-input" id="prompt-dialog-message"></label>
+        <input id="prompt-dialog-input" name="prompt-dialog-input">
+        <button type="submit">OK</button>
+    </form>
+</dialog>
+*/
